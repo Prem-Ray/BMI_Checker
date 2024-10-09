32eb2fed4c3059e4e0d11f42e00baf1247da2ab1
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2024-10-09 17:24:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('form') ;
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault() ;
+
+    const height =  parseInt(document.querySelector('#height').value) ;
+    const weight =  parseInt(document.querySelector('#weight').value) ;
+    const results = document.querySelector('#results') ;
+    const condition = document.querySelector('#condition') ;
+
+    if((height==='' || height<0 || isNaN(height)) && (weight==='' || weight<0 || isNaN(weight))){
+        results.innerHTML = `Please give valid height-weight, your height ${height} and weight ${weight}` ;
+    }
+    else if(height==='' || height<0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height, your height is ${height}` ;
+    }
+    else if(weight==='' || weight<0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight, your weight is ${weight}` ;
+    }
+    else{
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `<span>Your bmi is ${bmi}</span>` ;
+        if(bmi<18.6){
+            condition.innerHTML = `you are Underweight` ;
+        }
+        else if(bmi>=18.6 && bmi<=24.9){
+            condition.innerHTML = `you are at Normal Range` ;
+        }
+        else if(bmi>24.9){
+            condition.innerHTML = `you are OverWeight`
+        }
+    }
+
+    
+
+    
+    
+});
+
+
+
